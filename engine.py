cb43451086d80f1f065da4e088701832ede6f1e0
Ally <bagkingally@gmail.com>
2018-06-01 19:33:33 +0100

    Implemented alpha/beta pruning. Some profiling shows:
    No alpha/beta Depth 5: 50 seconds, 3.3m recursive calls to negaMax
    alpha/beta Depth 5: 3 seconds, 116k recursive calls to negaMax
    alpha/beta Depth 8: 59 seconds, 4m recursive calls to negaMax

--- engine.py
+++ engine.py
@@ -2,10 +2,12 @@ import random
 import cfg
 from core import Position
 
+# Constants
+LOW_SCORE = -(cfg.slots * cfg.stones)
 
 def play(pos):
     colour = 1 if cfg.enginePlayer == 0 else -1
-    (highScore, move) = negaMax(pos, cfg.depth, colour)
+    (highScore, move) = negaMax(pos, cfg.depth, LOW_SCORE, -LOW_SCORE, colour)
 
     if not move: # We were given depth 0
         move = random.choice(pos.listMoves())
@@ -14,25 +16,28 @@ def play(pos):
     print("Playing move {} with score {}".format(move, highScore))
     return (pos, move)
 
-def negaMax(pos, depth, colour):
+def negaMax(pos, depth, alpha, beta, colour):
     """Recursively find the best move via a negamax search"""
     candidateMoves = pos.listMoves()
     bestMove = None
     if depth == 0 or not candidateMoves:
         return (pos.score * colour, bestMove)
 
-    highScore = -(cfg.slots * cfg.stones)
+    highScore = LOW_SCORE
 
     for move in candidateMoves:
         candidatePos = pos.move(move)
         if candidatePos.playerToMove == pos.playerToMove:
             #print("{}: {} going deeper!".format(pos, move))
             # Search moves where we keep out turn deeper by leaving depth unchanged
-            score = negaMax(candidatePos, depth, colour)[0]
+            score = negaMax(candidatePos, depth, alpha, beta, colour)[0]
         else:
-            score = -negaMax(candidatePos, depth - 1, -colour)[0]
+            score = -negaMax(candidatePos, depth - 1, -beta, -alpha, -colour)[0]
         #highScore = max(score, highScore)
         if score > highScore:
             highScore, bestMove = score, move
+        alpha = max(alpha, score)
+        if alpha >= beta:
+            break
 
     return (highScore, bestMove)
